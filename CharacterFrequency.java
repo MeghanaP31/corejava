aad4d1a3f1b250bd4bbe15c120a2a8c2bcec449b
MeghanaP31 <meghana.p@techouts.co.in>
2026-09-23 15:45:21 +0530

    Update CharacterFrequency.java

--- CharacterFrequency.java
+++ CharacterFrequency.java
@@ -2,13 +2,18 @@ package javacollectionstest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CharacterFrequency {
     static void main() {
-        String input = "banana";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter an input: ");
+        String input = sc.next().toLowerCase();
+        
         Map<Character,Integer> hm=new HashMap<>();
+        
         for(char ch: input.toCharArray()){
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            hm.put(ch,hm.getOrDefault(ch,0)+1); // if the char already exists,it increments by 1
         }
         System.out.println(hm);
     }
